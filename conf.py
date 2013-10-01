091b5b7056746e4c6a35d1e5a65e92c2ce25ebb6
Pieter Swinkels <swinkels.pieter@yahoo.com>
2013-10-01 14:56:50 +0200

    Sets the deploy command (to deploy the site to blog.journeythatcounts.nl)

--- conf.py
+++ conf.py
@@ -110,7 +110,7 @@ REDIRECTIONS = []
 # "rsync -rav output/* joe@my.site:/srv/www/site"
 # And then do a backup, or ping pingomatic.
 # To do manual deployment, set it to []
-DEPLOY_COMMANDS = []
+DEPLOY_COMMANDS = ["rsync -Cavz output/ pieter@blog.journeythatcounts.nl:websites/blog-journey-that-counts/"]
 
 # Where the output site should be located
 # If you don't use an absolute path, it will be considered as relative
